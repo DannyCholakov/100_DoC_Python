f0447ed37e40fb403b7ce9d49cbacd6cfe91dc61
Daniel Cholakov <danielcholakov133@gmail.com>
2024-09-13 15:01:08 +0200

    Day One

---
+++ 01.BandNameGenerator/band_name_generator.py
@@ -0,0 +1,4 @@
+cityName = input("What is the name of the city you grew up in?:\n=> ")
+animalType = input("What type of animal did you have growing up?:\n=> ")
+
+print(f'Your band name is {cityName} {animalType}')
